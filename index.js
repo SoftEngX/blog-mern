86ba25c0f0a9680ba1cc4f6491cf1c18d8f68bd9
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-16 23:46:39 +0600

    feat: добавил registerValidation для /auth/register

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+
+
+import { registerValidation } from "./validations/auth.js";
 
 mongoose
-  .connect("mongodb+srv://bekbolsunakimov333_db_user:wwwmongobd@cluster0.hrwp71s.mongodb.net/?appName=Cluster0")
-  .then(() => console.log('DB ok'))
-  .catch((err) => console.log('DB error', err));
+  .connect(
+    "mongodb+srv://bekbolsunakimov333_db_user:@cluster0.hrwp71s.mongodb.net/?appName=Cluster0"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
 app.use(express.json());
 
 
-app.post("/auth/register", (req, res) => {
-  
+app.post("/auth/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  res.json({
+    success: true,
+  })
   
 });
 
